77a06911575c568047f82651a28aa3bb7281bf1c
='Johnson-ad <=>
2022-07-05 20:57:36 +0000

    Initialisation avec cote server

--- server/index.js
+++ server/index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
 
+mongoose.connect('mongodb://localhost:27017/learnMRN?readPreference=primary&appname=MongoDB%20Compass&ssl=false', { useNewUrlParser: true });
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+app.listen(3001, () => {
+    console.log('You are connected to port 3001');
 });
